8f9b6008847d579d459f11ae04c826955c734ea9
Kairsten Fay <kairsten.fay@gmail.com>
2019-08-29 08:11:12 -0700

    Add day 7 - power of three algo

---
+++ algoDaily/power-of-three.js
@@ -0,0 +1,8 @@
+// Given an integer num, write a method to determine if it is a power of 3.
+
+// console.log(powerOfThree(9));
+// // true
+
+const powerOfThree = (num) => {
+  return num % 3 === 0;
+};
